ebee1b69b2db028a49c0f4e7a629d9d2621ece23
AnnemiekNieboer <annemieknieboer@gmail.com>
2022-07-15 14:49:03 +0200

    Provider component made

--- src/context/TempProvider.js
+++ src/context/TempProvider.js
@@ -1,10 +1,24 @@
-import React, {createContext} from "react";
+import React, {createContext, useState} from "react";
+import kelvinToCelsius from "../helpers/kelvinToCelsius";
+import kelvinToFahrenheit from "../helpers/kelvinToFahrenheit";
 
 export const TempContext = createContext(null);
 
 function TempContextProvider({children}) {
+    const [selectedMetric, toggleSelectedMetric] = useState('celcius');
+
+    function toggleTemp() {
+        if (selectedMetric === 'celsius') {
+            toggleSelectedMetric('fahrenheit');
+        } else {
+            toggleSelectedMetric('celcius')
+        }
+    }
     return (
-        <TempContext.Provider value={}>
+        <TempContext.Provider value={{
+            toggleTemp: toggleTemp,
+            kelvinToMetric: selectedMetric === 'celsius' ? kelvinToCelsius : kelvinToFahrenheit,
+        }}>
             {children}
         </TempContext.Provider>
     )
